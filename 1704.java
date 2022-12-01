a7016701fc6e81c75fc6bbe622842dc553a30b45
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2022-12-01 20:25:03 +0530

    Update 1704.java

--- 1704.java
+++ 1704.java
@@ -35,3 +35,28 @@ class Solution {
             
     }
 }
+
+//Another possible solution
+
+class Solution {
+    public boolean vowel(char c){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
+          c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+            return true;
+        return false;
+    }
+    public boolean halvesAreAlike(String s) {
+        int i=0;
+        int j=s.length()-1;
+        int a=0;
+        int b=0;
+        while(i<j){
+            if(vowel(s.charAt(i++)))
+                a++;
+            if(vowel(s.charAt(j--)))
+                b++;
+            
+        }
+        return (a==b);
+    }
+}
