52942af6b01f55512a4a310f24bc7f590d0e5266
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2022-12-01 20:06:04 +0530

    Create 1704.java
    NOTE: using array as a frequency table

---
+++ 1704.java
@@ -0,0 +1,37 @@
+/* 
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+ 
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+*/
+
+
+class Solution {
+    public boolean halvesAreAlike(String si) {
+        String s=si.toLowerCase();
+        
+        int[] a1=new int[26];
+        int[] a2=new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            if(i<s.length()/2)
+                a1[s.charAt(i)-'a']++;
+            else
+                a2[s.charAt(i)-'a']++;
+        }
+        
+        if(a1[0]+ a1[4]+ a1[8]+ a1[14]+a1[20] != a2[0]+ a2[4]+ a2[8]+ a2[14]+ a2[20])
+           return false;
+        
+        return true;
+            
+    }
+}
