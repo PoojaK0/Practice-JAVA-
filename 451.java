167d6678e33d44690faf5ee0b26a9ae1fa369ef6
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2022-12-03 23:02:19 +0530

    Update 451.java

--- 451.java
+++ 451.java
@@ -11,6 +11,19 @@ Input: s = "tree"
 Output: "eert"
 Explanation: 'e' appears twice while 'r' and 't' both appear once.
 So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 105
+s consists of uppercase and lowercase English letters and digits
 */
 
 
