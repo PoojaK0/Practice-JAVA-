313ee1b5c8caa127c7ce3298b3d7b5de25c6097d
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2022-12-18 19:31:32 +0530

    Create 739.java

---
+++ 739.java
@@ -0,0 +1,41 @@
+/*
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+*/
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+      int len = temperatures.length;
+        int[] result = new int[len];
+        if (len <= 1) {
+            return result;
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return result;
+
+    }
+}
