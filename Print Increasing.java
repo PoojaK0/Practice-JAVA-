4628fbef60036ea4cd63fdc080712babb87098ef
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-02-26 14:11:45 +0530

    Create Print Increasing.java

---
+++ Print Increasing.java
@@ -0,0 +1,35 @@
+/*
+1. You are given a positive number n. 
+2. You are required to print the counting from 1 to n.
+3. You are required to not use any loops. Complete the body of print Increasing function to achieve it. Don't change the signature of the function.
+
+Input Format
+A number n
+Output Format
+1
+2
+3
+..
+n
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n>1)
+        printIncreasing(n-1);
+        System.out.println(n);
+        return;
+    }
+
+}
