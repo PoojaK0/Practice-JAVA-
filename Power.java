dffcaefd7402f19ce3c1fe2ae91abb84e1bf177c
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-03-01 17:56:48 +0530

    Update and rename Power-linear.java to Power.java

--- Power.java
+++ Power.java
@@ -30,3 +30,35 @@ public class Main {
     }
 
 }   
+
+
+
+// power-log
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(power(x,n));
+    }
+
+    public static int power(int x, int n){
+        if(n==0)
+        return 1;
+        
+        int ret=power(x,n/2);
+        int ans=ret*ret;
+        
+        if(n%2==1)
+        ans=ans*x;
+        
+        return ans;
+    }
+
+}
