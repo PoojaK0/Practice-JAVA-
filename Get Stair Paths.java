5fca115dd8952644a75a3984bb3dc871c49b1385
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-03-09 14:58:25 +0530

    Update Get Stair Paths.java

--- Get Stair Paths.java
+++ Get Stair Paths.java
@@ -54,3 +54,35 @@ public class Main {
     }
 
 }
+
+
+
+//dp
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n+1];
+        arr[0]=1;
+       
+        for(int i=1; i<=n; i++){
+            if(i>= 1)
+            arr[i]+=arr[i-1];
+            
+            if(i>=2)
+            arr[i]+=arr[i-2];
+            
+            if(i>=3)
+            arr[i]+=arr[i-3];
+        }
+        
+        System.out.println(arr[n]);
+    }
+
+}
