c84d8f78ceb10f7fd9ec85350f8a1e23610042d7
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-04-15 19:33:31 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int n) {
+        int r=-1;
+
+        for(int i=0; i<arr.length; i++)
+            if(arr[i][arr[0].length-1] >= n){
+                r=i;
+                break;
+                }
+            
+        if(r==-1)
+        return false;
+
+        for(int j=0; j<arr[0].length; j++)
+            if(arr[r][j] == n)
+                return true;
+            
+        
+        return false;
+    }
+}
