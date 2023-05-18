5e95ab244726efc18d67d70fd779fc12c2f640d2
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-05-18 20:57:48 +0530

    Create 61.java

---
+++ 61.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if( head == null || head.next==null)
+        return head;
+
+        int len=0;
+        ListNode curr=head;
+        while( curr.next != null){
+            curr=curr.next;
+            len++;
+        }
+        len++;
+
+        if(k%len == 0)
+        return head;
+
+        ListNode last=curr;
+        curr=head;
+        k=k%len;
+        len=len-k;
+
+        while(len-- > 1){
+            curr=curr.next;
+        }
+        
+        ListNode next=curr.next;
+        curr.next=null;
+        last.next=head;
+        return next;
+    }
+}
+
+
+
+//
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+   public ListNode rotateRight(ListNode head, int n) {
+    if (head==null||head.next==null) return head;
+    ListNode dummy=new ListNode(0);
+    dummy.next=head;
+    ListNode fast=dummy,slow=dummy;
+
+    int i;
+    for (i=0;fast.next!=null;i++)//Get the total length 
+    	fast=fast.next;
+    
+    for (int j=i-n%i;j>0;j--) //Get the i-n%i th node
+    	slow=slow.next;
+    
+    fast.next=dummy.next; //Do the rotation
+    dummy.next=slow.next;
+    slow.next=null;
+    
+    return dummy.next;
+}
+}
