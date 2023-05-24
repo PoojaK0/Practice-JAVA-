05d401e0866034451c48a6b38da27764e9f9eb99
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-05-24 23:42:09 +0530

    Create 485.java

---
+++ 485.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int ans=0;
+        for(int i : nums){
+        if(i==1){
+        max++;
+        ans=Math.max(max,ans);
+        }
+        else
+        max=0;
+        }
+
+        return ans;
+}
+    }
+    
