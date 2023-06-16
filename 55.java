47cc45dc23122860993a5e8b86a39d9ccea666f8
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-06-16 20:30:59 +0530

    Update 55.java

--- 55.java
+++ 55.java
@@ -30,3 +30,19 @@ class Solution {
 return true;
     }
 }
+
+
+//
+class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {return true;}
+
+        int coverRange = 0;
+        for (int i = 0; i <= coverRange; i++) {
+            coverRange = Math.max(coverRange, i + nums[i]);
+            if (coverRange >= nums.length - 1) {return true;}
+        }
+
+        return false;
+    }
+}
