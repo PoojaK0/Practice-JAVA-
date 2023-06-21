6e43dc9498896f454ba329f2c2a8b7becdb1c00e
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-06-21 20:43:10 +0530

    Update 14.java

--- 14.java
+++ 14.java
@@ -14,3 +14,21 @@ class Solution {
         return sb.toString();
     }
 }
+
+//
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+       String f= strs[0];
+        String l= strs[strs.length-1];
+        
+        int c=0;
+        while(c<f.length()){
+            if( f.charAt(c) == l.charAt(c))
+                c++;
+            else
+                break;
+        }
+        return c==0?"":f.substring(0,c);
+    }
+}
