afd19448401b7eb938a5ebb8a5bae9b7e4c4e967
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-06-22 14:56:54 +0530

    Update 151.java

--- 151.java
+++ 151.java
@@ -1,8 +1,7 @@
 //Brute
 class Solution {
     public String reverseWords(String s) {
-      
-        String si=s.trim();
+
         int i=0;
 
         List<String> arr= new ArrayList<>();
@@ -16,25 +15,43 @@ class Solution {
                 arr.add(sb.toString());
                  sb=new StringBuilder();
             }
-
+            
+             //for last element where space may or may not be present
             if(i==si.length()-1){
-            arr.add(sb.toString());
-            sb=new StringBuilder();
+                arr.add(sb.toString());
+                sb=new StringBuilder();
             }
+            
             i++;
         }
-        System.out.println(arr);
-        // System.out.println(arr.get(arr.size()-1));
 
         for(int j=arr.size()-1; j>=0; j--){
 
             if(!arr.get(j).equals("")){
                 sb.append(arr.get(j));
                 sb.append(" ");
-                }
-            // System.out.println(!arr.get(j).equals(" "));
+            }
         }
 
+        return sb.toString().trim();
+    }
+}
+
+//OR
+class Solution {
+    public String reverseWords(String s) {
+
+        String si=s.trim();
+        //Splits the string wherever " " is present 
+        //" +" means spliting even if multiple spaces are present
+
+        String[] arr= si.split(" +");
+        StringBuilder sb=new StringBuilder();
+        
+        for(int i=arr.length-1; i>=0; i--){
+            sb.append(arr[i]);
+            sb.append(" ");
+        }
         return sb.toString().trim();
     }
 }
