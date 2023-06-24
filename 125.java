387df43fea667b1814653b92f2a353dceab59e7f
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-06-24 23:04:38 +0530

    Update 125.java

--- 125.java
+++ 125.java
@@ -22,3 +22,27 @@ class Solution {
         return s1.toString().equals(s2.toString());
     }
 }
+
+
+
+//
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(!Character.isLetterOrDigit(s.charAt(i)))
+                i++;
+            else if(!Character.isLetterOrDigit(s.charAt(j)))
+                j--;
+            else if(Character.isLetterOrDigit(s.charAt(i)) && Character.isLetterOrDigit(s.charAt(j))){
+                if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)) )
+                    return false;
+                else
+                    {i++;
+                    j--;}
+            }
+        }
+        return true;
+    }
+}
