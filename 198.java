08a3598c472516fef92859f9f4f41909c0a47b5b
Pooja Kumari <116003753+PoojaK0@users.noreply.github.com>
2023-06-27 16:18:11 +0530

    Update 198.java

--- 198.java
+++ 198.java
@@ -44,8 +44,8 @@ class Solution {
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
 
-        if(nums.length==2)
-            return Math.max(nums[0],nums[1]);
+        // if(nums.length==2)
+        //     return Math.max(nums[0],nums[1]);
 
         for(int i=2; i<dp.length; i++){
             dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
